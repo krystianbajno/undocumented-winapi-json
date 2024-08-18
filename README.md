2ca7ad8a40d545ce18b6a35b840288c5496ecb32
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-18 17:44:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phnt-winapi-json
+Undocumented WinAPI collector from [PHNT](https://github.com/winsiderss/phnt) repository. Used for [winapi-search](https://github.com/krystianbajno/winapi-search) project.
---
+++ src/main.rs
@@ -0,0 +1,93 @@
+use serde_derive::Serialize;
+use std::fs::File;
+use std::io::Write;
+use reqwest;
+use regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Serialize)]
+struct Module {
+    module_name: String,
+    phnt_link: String,
+    functions: Vec<Function>,
+}
+
+#[derive(Serialize)]
+struct Function {
+    function_name: String,
+    ret_type: String,
+    params: Vec<String>,
+}
+
+fn download_file(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::blocking::get(url)?;
+    Ok(response.text()?)
+}
+
+fn parse_function_definitions(content: &str) -> Vec<Function> {
+    let mut functions = Vec::new();
+    
+    let re = Regex::new(r"NTSYSAPI\s+(?P<ret_type>\w[\w\s\*]+)\s+NTAPI\s+(?P<name>\w+)\s*\((?P<params>[^)]*)\)").unwrap();
+    
+    for cap in re.captures_iter(content) {
+        let ret_type = cap["ret_type"].trim().to_string();
+        let function_name = cap["name"].to_string();
+        let params = cap["params"]
+            .split(',')
+            .map(|p| p.trim().to_string())
+            .collect::<Vec<String>>();
+
+        functions.push(Function {
+            function_name,
+            ret_type,
+            params,
+        });
+    }
+    
+    functions
+}
+
+fn main() {
+    let files = vec![
+        "ntbcd.h", "ntdbg.h", "ntexapi.h", "ntgdi.h", "ntimage.h", "ntioapi.h", "ntkeapi.h", 
+        "ntldr.h", "ntlpcapi.h", "ntmisc.h", "ntmmapi.h", "ntnls.h", "ntobapi.h", "ntpebteb.h",
+        "ntpfapi.h", "ntpnpapi.h", "ntpoapi.h", "ntpsapi.h", "ntregapi.h", "ntrtl.h", "ntsam.h",
+        "ntseapi.h", "ntsmss.h", "ntsxs.h", "nttmapi.h", "nttp.h", "ntwmi.h", "ntwow64.h", 
+        "ntxcapi.h", "ntzwapi.h", "phnt.h", "phnt_ntdef.h", "phnt_windows.h", "subprocesstag.h",
+        "usermgr.h", "winsta.h"
+    ];
+
+    let base_url = "https://raw.githubusercontent.com/winsiderss/phnt/master/";
+    let phnt_base_link = "https://github.com/winsiderss/phnt/blob/master/";
+
+    let mut modules = HashMap::new();
+
+    for file_name in files {
+        let file_url = format!("{}{}", base_url, file_name);
+        let phnt_link = format!("{}{}", phnt_base_link, file_name);
+        
+        match download_file(&file_url) {
+            Ok(content) => {
+                let module_name = "ntdll.dll".to_string();
+                let functions = parse_function_definitions(&content);
+
+                if !functions.is_empty() {
+                    let module = modules.entry(module_name.clone()).or_insert(Module {
+                        module_name,
+                        phnt_link: phnt_link.clone(),
+                        functions: Vec::new(),
+                    });
+
+                    module.functions.extend(functions);
+                }
+            },
+            Err(err) => {
+                eprintln!("Failed to download or parse {}: {}", file_name, err);
+            }
+        }
+    }
+
+    let res = serde_json::to_string_pretty(&modules.values().collect::<Vec<&Module>>()).unwrap();
+    let mut handler = File::create("./output.json").unwrap();
+    handler.write_all(res.as_bytes()).unwrap();
+}
